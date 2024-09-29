9bb991c04d4708308ddecc63c9adbe4b2c84e2e8
jordan <jordanelcracken@gmail.com>
2024-09-29 13:59:44 -0500

    feat: add info fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import AboutView from "@/components/about";
+import InfoFixed from "@/components/info-fixed";
 import PresentationView from "@/components/presentation";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="relative">
       <PresentationView />
       <AboutView />
+      <InfoFixed/>
     </main>
   )
 }
---
+++ src/components/info-fixed.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InfoFixed = () => {
+  return (
+    <div className='fixed bottom-[100px] right-0 rotate-90'>
+      <p className='tracking-wider text-[#8e98a7]'>jordan.huaman.com</p>
+    </div>
+  )
+}
+
+export default InfoFixed
