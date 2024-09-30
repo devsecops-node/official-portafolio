489f0067aa749d1f4a644e047288216b1fa7e9dc
jordan <jordanelcracken@gmail.com>
2024-09-29 19:17:57 -0500

    feat: add hint component

--- src/components/presentation/links.tsx
+++ src/components/presentation/links.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import styles from "@/styles/anchor.module.css"
+import Hint from '../hint'
 
 const Links = ({ name, id }: { name: string, id: number }) => {
   const [monted, setMounted] = useState(false)
@@ -12,17 +13,19 @@ const Links = ({ name, id }: { name: string, id: number }) => {
 
   if (!monted) return <div>loading</div>
   else {
-    aboutView = document.getElementById('About')
+    aboutView = document.getElementById(name)
   }
 
   return (
     <div className='relative'>
-      <a
-        onClick={() => aboutView?.scrollIntoView({ behavior: "smooth" })}
-        className={`flex font-ppELight text-[12px] opacity-[0.7] tracking-wider hover:opacity-[1] transition-all duration-150 cursor-pointer  ${styles.line}`}>
-        <p className='text-[var(--text-primary)] font-ppBold'>0{id}.</p>
-        <p>{name}</p>
-      </a>
+      <Hint label={name}>
+        <a
+          onClick={() => aboutView?.scrollIntoView({ behavior: "smooth" })}
+          className={`flex font-ppELight text-[12px] opacity-[0.7] tracking-wider hover:opacity-[1] transition-all duration-150 cursor-pointer  ${styles.line}`}>
+          <p className='text-[var(--text-primary)] font-ppBold'>0{id}.</p>
+          <p>{name}</p>
+        </a>
+      </Hint>
     </div>
   )
 }
