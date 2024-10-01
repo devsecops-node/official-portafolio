dbb77d00e0e1ed450d2862a7561e2f84cafc0835
jordan <jordanelcracken@gmail.com>
2024-10-01 14:58:24 -0500

    feat: add some fixed

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -10,6 +10,16 @@ const ExperienceView = () => {
   const { apper } = useIntersectionObserver({ observar: "Experience" })
 
   return (
+    <div>
+      <div id='Experience' className={`h-screen flex items-center justify-center ${apper ? styles.main : "opacity-0"}`} >
+        <div className='w-[80%] md:w-[50%]'>
+          <TitleSection sectionNumber='02.' sectionTitle="Where I've work " />
+          <h1 className='text-font-ppSemiBold text-center text-[30px]'>Comming soon...</h1>
+        </div>
+      </div>
+    </div>
+  )
+  return (
     <div id='Experience' className={`h-screen flex items-center justify-center ${apper ? styles.main : "opacity-0"}`} >
       <div className='w-[80%] md:w-[50%]'>
         <TitleSection sectionNumber='02.' sectionTitle="Where I've work " />
