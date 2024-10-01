9a6fe128ae77a68dee03916d7922137d27c2e458
jordan <jordanelcracken@gmail.com>
2024-10-01 15:21:02 -0500

    feat: add portafolio

--- src/components/info-fixed-l.tsx
+++ src/components/info-fixed-l.tsx
@@ -8,11 +8,19 @@ const InfoFixedLeft = () => {
     <div className={`fixed bottom-0 left-0 hidden sm:block px-[10px]  ${styles.apper}`}>
       <div className='flex flex-col items-center gap-2 h-full px-3'>
         <section className='flex gap-3 flex-col'>
-          <Github className='text-light_slate transition-all duration-[0.5s] hover:text-primary cursor-pointer hover:translate-y-[-5px]' size={23} />
-          <Instagram className='text-light_slate transition-all duration-[0.5s] hover:text-primary cursor-pointer hover:translate-y-[-5px]' size={23} />
-          <Twitter className='text-light_slate transition-all duration-[0.5s] hover:text-primary cursor-pointer hover:translate-y-[-5px]' size={23} />
-          <Linkedin className='text-light_slate transition-all duration-[0.5s] hover:text-primary cursor-pointer hover:translate-y-[-5px]' size={23} />
-          <SquareArrowOutUpRight className='text-light_slate transition-all duration-[0.5s] hover:text-primary cursor-pointer hover:translate-y-[-5px]' size={23} />
+          <a href="https://github.com/orgs/devops-jordan/repositories" target='_blank'><Github className='text-light_slate transition-all duration-[0.5s] hover:text-primary cursor-pointer hover:translate-y-[-5px]' size={23} /></a>
+          <a href="https://www.instagram.com/jordanhuaman_g/" target='_blank'>
+            <Instagram className='text-light_slate transition-all duration-[0.5s] hover:text-primary cursor-pointer hover:translate-y-[-5px]' size={23} />
+          </a>
+          <a href="https://x.com/alexand99723966" target='_blank'>
+            <Twitter className='text-light_slate transition-all duration-[0.5s] hover:text-primary cursor-pointer hover:translate-y-[-5px]' size={23} />
+          </a>
+          <a href='https://www.linkedin.com/in/alexander-huaman-4992b8205/' target='_blank'>
+            <Linkedin className='text-light_slate transition-all duration-[0.5s] hover:text-primary cursor-pointer hover:translate-y-[-5px]' size={23} />
+          </a>
+          <a href="https://official-portafolio.vercel.app/" target='_blank'>
+            <SquareArrowOutUpRight className='text-light_slate transition-all duration-[0.5s] hover:text-primary cursor-pointer hover:translate-y-[-5px]' size={23} />
+          </a>
         </section>
         <div className='bg-light_slate w-[2px] h-[200px]' />
       </div>
